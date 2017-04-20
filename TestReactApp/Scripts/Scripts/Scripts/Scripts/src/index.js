64c3220ba9ecc814525d8355580605ddfa7e8775
Slepov.V <redheadguy666@gmail.com>
2017-04-20 14:13:33 +0300

    Удалены скрипты [2]

--- TestReactApp/Scripts/Scripts/Scripts/Scripts/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const React = require("react");
-const ReactDOM = require("react-dom");
-const well_1 = require("./components/well");
-ReactDOM.render(React.createElement("div", null, React.createElement(well_1.Well, { text: "Well, well, well" })), document.getElementById("body"));
-//# sourceMappingURL=index.js.map 
-//# sourceMappingURL=index.js.map 
-//# sourceMappingURL=index.js.map
--- TestReactApp/Scripts/Scripts/src/components/well.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const React = require("react");
-class Well extends React.Component {
-    render() {
-        return React.createElement("div", {className: "well"}, this.props.text);
-    }
-}
-exports.Well = Well;
-//# sourceMappingURL=well.js.map
