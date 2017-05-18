c680501522ba2b44cf43639ab131036d9c8aef98
Slepov.V <redheadguy666@gmail.com>
2017-05-18 18:10:08 +0300

    Refactoring[2]

--- TestReactApp/Scripts/dist/bundle.js
+++ TestReactApp/Scripts/dist/bundle.js
@@ -22034,9 +22034,6 @@ class Building extends React.Component {
         });
         this.props.handleTree(equipmentMassive);
     }
-    countEquipmentInRoom(roomEquipment) {
-        //this.props.handleTree(roomEquipment);
-    }
     render() {
         var rooms = this.props.rooms.map((room) => React.createElement(Room_1.Room, { id: room.roomId, name: room.name, key: room.roomId, equipmentInRoom: room.equipment, buildingCallback: this.props.handleTree, icon: "/Content/Images/blue-folder.ico" }));
         return (React.createElement("div", null,
